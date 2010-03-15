75ad909f1270543c2ba531401e6a886d4d88df1c
Adam Greig <random@randomskk.net>
2010-03-15 21:33:55 +0000

    Added in maths. Could probably still do with optimisation.

--- calc.js
+++ calc.js
@@ -213,8 +213,92 @@ function calc_update() {
         return;
     
     // Do some maths
+    mb = mb / 1000.0;
+    mp = mp / 1000.0;
 
+    var ascent_rate = 0;
+    var burst_altitude = 0;
+    var time_to_burst = 0;
+    var neck_lift = 0;
+    var launch_radius = 0;
+    var launch_volume = 0;
 
+    var burst_volume = (4.0/3.0) * Math.PI * Math.pow(bd / 2.0, 3);
+
+    if(tba_set) {
+        launch_volume = burst_volume * Math.exp((-tba) / adm);
+        launch_radius = Math.pow((3*launch_volume)/(4*Math.PI), (1/3));
+    } else if(tar_set) {
+        var a = 9.81 * (rho_a - rho_g) * (4.0 / 3.0) * Math.PI;
+        var b = -0.5 * Math.pow(tar, 2) * cd * rho_a * Math.PI;
+        var c = 0;
+        var d = - (mp + mb) * 9.81;
+
+        var f = (((3*c)/a) - (Math.pow(b, 2) / Math.pow(a,2)) / 3.0);
+        var g = (((2*Math.pow(b,3))/Math.pow(a,3)) - ((9*b*c)/(Math.pow(a,2))) + ((27*d)/a) / 27.0);
+        var h = (Math.pow(g,2) / 4.0) + (Math.pow(f,3) / 27.0);
+
+        if(h>0) {
+            // One real root
+            var R = (-0.5 * g) + Math.sqrt(h);
+            var S = Math.pow(R, 1.0/3.0);
+            var T = (-0.5 * g) - Math.sqrt(h);
+            var U = Math.pow(T, 1.0/3.0);
+            launch_radius = (S+U) - (b/(3*a));
+        } else if(f==0 && g==0 && h==0) {
+            // Three real and equal roots
+            // Will this ever even happen?
+            launch_radius = -1 * Math.pow(d/a, 1.0/3.0);
+        } else if(h <= 0) {
+            // Three real and different roots
+            // What the hell do we do?!
+            // It needs trig! fffff
+            var i = Math.sqrt((Math.pow(g,2)/4.0) - h);
+            var j = Math.pow(i, 1.0/3.0);
+            var k = Math.acos(-g / (2*i));
+            var L = -1 * j;
+            var M = Math.cos(K/3.0);
+            var N = Math.sqrt(3) * Math.sin(K/3.0);
+            var P = (b/(3*a)) * -1;
+            var r1 = 2*j*Math.cos(k/3.0) - (b/(3*a));
+            var r2 = L * (M + N) + P;
+            var r3 = L * (M - N) + P;
+
+            alert("Three possible solutions found: " + r1 + ", " + r2 + ", " + r3);
+            
+            if(r1 > 0) {
+                launch_radius = r1;
+            } else if(r2 > 0) {
+                launch_radius = r2;
+            } else if(r3 > 0) {
+                launch_radius = r3;
+            }
+        } else {
+            // No real roots
+        }
+    }
+
+    var launch_area = Math.PI * Math.pow(launch_radius, 2);
+    var launch_volume = (4.0/3.0) * Math.PI * Math.pow(launch_radius, 3);
+    var density_difference = rho_a - rho_g;
+    var gross_lift = launch_volume * density_difference;
+    neck_lift = (gross_lift - mb) * 1000;
+    var total_mass = mp + mb;
+    var free_lift = (gross_lift - total_mass) * 9.81;
+    ascent_rate = Math.sqrt(free_lift / (0.5 * cd * launch_area * rho_a));
+    var volume_ratio = launch_volume / burst_volume;
+    burst_altitude = -(adm) * Math.log(volume_ratio);
+    time_to_burst = (burst_altitude / ascent_rate) / 60.0;
+
+    ascent_rate = Math.round(ascent_rate * 100) / 100;
+    burst_altitude = Math.round(burst_altitude);
+    time_to_burst = Math.round(time_to_burst);
+    neck_lift = Math.round(neck_lift);
+
+    document.getElementById('ar').innerHTML = ascent_rate + " m/s";
+    document.getElementById('ba').innerHTML = burst_altitude + " m";
+    document.getElementById('ttb').innerHTML = time_to_burst + " min";
+    document.getElementById('nl').innerHTML = neck_lift + " g";
 }
 
 function calc_init() {
@@ -222,4 +306,5 @@ function calc_init() {
     for(var i in ids) {
         document.getElementById(ids[i]).onchange = calc_update;
     }
+    calc_update();
 }
